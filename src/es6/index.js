0132ab9f957145baf3bbe57de7bee0ab09001b4c
jatamayo <jtamayoalejandro@gmail.com>
2020-07-07 22:58:50 -0600

    ADD: javascript promesas

--- src/es6/index.js
+++ src/es6/index.js
@@ -23,3 +23,20 @@ let joinTeam = ['karem', 'beatriz', 'tamayo', ...team1, ...team2]
 console.log(joinTeam)
 
 
+// Promesas
+const helloPromise = (()=> {
+    return new Promise((resolve, reject) => {
+        if(false){
+            resolve('success');
+        }else{
+            reject('error');
+        }
+    })
+})
+helloPromise()
+    .then((response)=>{
+        console.log(response);
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
