6a04f53d32f14c779c139262b90252251acdfffe
jatamayo <jtamayoalejandro@gmail.com>
2020-07-07 22:44:04 -0600

    ADD: ecma6 nuevos complementos

---
+++ src/es6/index.js
@@ -0,0 +1,25 @@
+// JavaScript Normal
+function newFunction(name, age, country){
+    var name = name || 'jose';
+    var age = age || 28;
+    var country = country || 'Guatemala';
+    console.log(name, age, country);
+}
+
+
+// ECMAScript 6
+function newFunction2(name = 'karem', age = 50, country  = 'Guatemala'){
+    console.log(name, age, country);
+}
+
+newFunction2();
+newFunction2('jose', 28, 'MX');
+
+// 
+let team1 = ['jose', 'alejandro', 'tamayo']
+let team2 = ['gloria', 'soraida', 'villagran']
+
+let joinTeam = ['karem', 'beatriz', 'tamayo', ...team1, ...team2]
+console.log(joinTeam)
+
+
