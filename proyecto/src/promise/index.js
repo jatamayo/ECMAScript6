3571414db6c13bd92cc3cf7b86332fb1b12924ec
jatamayo <jtamayoalejandro@gmail.com>
2020-07-19 11:41:25 -0600

    promesas

--- proyecto/src/promise/index.js
+++ proyecto/src/promise/index.js
@@ -17,6 +17,26 @@ somethigWillHappen()
     })
 
 /********************************************* */
+const promesaTesting = () => {
+    return new Promise((resolve, reject)=>{
+        if(false){
+            resolve("uffas creamo una promesa con exito");
+        }else{
+            const error = new Error('upps error');
+            reject(error);
+        }
+    })
+}
+promesaTesting()
+    .then((response)=>{
+        console.log(response);
+    })
+    .catch((error)=>{
+        console.error(error);
+    })
+
+
+/********************************************* */
 
 const somethingWillHappen2 = () => {
     return new Promise((resolve, reject)=> {
@@ -38,3 +58,14 @@ somethingWillHappen2()
     .catch((error)=>{
         console.log(error);
     })
+
+Promise.all([somethigWillHappen(), somethingWillHappen2()])
+    .then((response)=>{
+        console.log(reponse);
+    })
+    .catch((error)=>{
+        console.error(error);
+    })
+
+
+/************************************************ */
